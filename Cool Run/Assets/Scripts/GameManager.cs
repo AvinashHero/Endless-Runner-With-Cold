a150d2559fdf8236d8fd128e1ade01e72e8536d8
AvinashHero <postariyaavinash@gmail.com>
2019-08-12 18:35:30 +0530

    FirstUpload(12-08-19)

---
+++ Cool Run/Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private const int COIN_SCORE_AMOUNT = 5;
+    public static GameManager Instance { set; get; }
+
+    public bool IsDead { set; get; }
+    private bool isGameStarted = false;
+    private PlayerMotor motor;
+
+    //UI and the UI fields
+    public Animator canvasGroup,menuTitleAnim;
+    public Text scoreText, coinText, modifierText, highScore;
+    private float score, coinScore, modifierScore;
+    private int lastScore;
+
+    //Death menu
+    public Animator deathAnimator;
+    public Text deathScoreText, deathCoin;
+
+    private void Awake()
+    {
+        Instance = this;
+        modifierScore = 1;
+        motor = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotor>();
+        scoreText.text = scoreText.text = score.ToString("0");
+        coinText.text = coinScore.ToString("0");
+        modifierText.text = "x" + modifierScore.ToString("0.0");
+        highScore.text = PlayerPrefs.GetInt("Hiscore").ToString();
+
+    }
+
+    private void Update()
+    {
+        if(MobileInput.Instance.Tap && !isGameStarted)
+        {
+            isGameStarted = true;
+            motor.StartGame();
+            FindObjectOfType<GleciarSpawn>().IsScrolling = true;
+            FindObjectOfType<CameraMotor>().IsMoving = true;
+            canvasGroup.SetTrigger("Show");
+            menuTitleAnim.SetTrigger("Hide");
+        }
+
+        if(isGameStarted && !IsDead)
+        {
+            score += (Time.deltaTime * modifierScore);
+            if(lastScore != (int)score)
+            {
+                lastScore = (int)score;
+                scoreText.text = score.ToString("0");
+            }
+        }   
+    }
+
+    public void GetCoin()
+    {
+        coinScore++;
+        coinText.text = coinScore.ToString("0");
+        score += COIN_SCORE_AMOUNT;
+        scoreText.text = scoreText.text = score.ToString("0");
+    }
+  
+
+    public void UpdateModifier(float modifierAmount)
+    {
+        modifierScore = 1.0f + modifierAmount;
+        modifierText.text = "x" + modifierScore.ToString("0.0");
+    }
+
+    public void OnPlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void OnDeath()
+    {
+        IsDead = true;
+        FindObjectOfType<GleciarSpawn>().IsScrolling = false;
+        deathScoreText.text = score.ToString("0");
+        deathCoin.text = coinScore.ToString("0");
+        deathAnimator.SetTrigger("Death");
+        canvasGroup.SetTrigger("Hide");
+
+        //Check if this is a highscore
+        if (score > PlayerPrefs.GetInt("Hiscore"))
+        {
+            float s = score;
+            if (s % 1 == 0)
+                s += 1;
+
+            PlayerPrefs.SetInt("Hiscore", (int)s);
+        }
+            
+    }
+}
+
---
+++ Cool Run/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private const float LANE_DISTANCE = 2.5f;
+    private const float Turn_Speed = 0.05f;
+
+    //
+    private bool isRunning = false;
+
+    //Movement
+    private CharacterController controller;
+    private float jumpForce = 4f;
+    private float gravity = 12.0f;
+    private float verticalVelocity;
+    
+
+    //Animation
+    private Animator animator;
+
+    private int desiredLane = 1; // 0 = Left, 1 = Middle, 2 = Right
+
+    //Speed Modifier
+    private float originalSpeed = 7.0f;
+    private float speed;
+    private float speedIncreaseLastTick;
+    private float speedIncreaseTime = 2.5f;
+    private float speedIncreaseAmount = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = originalSpeed;
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        SetAnimator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isRunning)
+            return;
+
+        if(Time.time - speedIncreaseLastTick > speedIncreaseTime)
+        {
+            speedIncreaseLastTick = Time.time;
+            speed += speedIncreaseAmount;
+            GameManager.Instance.UpdateModifier(speed - originalSpeed);
+
+        }
+        //Gather the inputs on which lane we should be
+        if (MobileInput.Instance.SwipeLeft)
+            MoveLane(false);
+
+        if (MobileInput.Instance.SwipeRight)
+            MoveLane(true);
+
+        //Calculate where we should be in the future
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+        
+        if(desiredLane == 0)
+              targetPosition += Vector3.left * LANE_DISTANCE;
+       
+        else if(desiredLane == 2)
+              targetPosition += Vector3.right * LANE_DISTANCE;
+    
+        
+        // let's calculate our move delta
+
+        Vector3 moveVector = Vector3.zero;
+
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+
+        bool isGrounded = IsGrounded();
+        animator.SetBool("isGrounded", isGrounded);
+        if(isGrounded)//isGrounded is true
+        {
+            verticalVelocity = -0.1f;
+
+            if(MobileInput.Instance.SwipeUp)
+            {
+                //jump
+                animator.SetTrigger("Jump");
+                verticalVelocity = jumpForce;
+            }
+            else if(MobileInput.Instance.SwipeDown)
+            {
+                //Slide
+                StartSlide();
+                Invoke("StopSlide", 1.0f);
+            }
+        }
+        else
+        {
+            verticalVelocity -= (gravity * Time.deltaTime);
+
+            //Fast falling down
+            if(MobileInput.Instance.SwipeDown)
+            {
+                verticalVelocity = -jumpForce;
+            }
+        }
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        //Move the Character
+        controller.Move(moveVector * Time.deltaTime);
+
+        //Rotate the player where he is going
+        Vector3 dir = controller.velocity;
+        if(dir != Vector3.zero)
+        {
+            dir.y = 0;
+            transform.forward = Vector3.Lerp(transform.forward, dir, Turn_Speed);
+        }
+       
+    }
+
+    private void StartSlide()
+    {
+        animator.SetBool("isSliding", true);
+        controller.height /= 2;
+        controller.center = new Vector3(controller.center.x, controller.center.y / 2, controller.center.z);
+    }
+
+    private void StopSlide()
+    {
+        animator.SetBool("isSliding", false);
+        controller.height *= 2;
+        controller.center = new Vector3(controller.center.x, controller.center.y * 2, controller.center.z);
+
+    }
+
+    private void MoveLane(bool goingRight)
+    {
+        desiredLane += (goingRight) ? 1 : -1;
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+    }
+
+    private bool IsGrounded()
+    {
+        Ray groundRay = new Ray(new Vector3(controller.bounds.center.x,
+            (controller.bounds.center.y - controller.bounds.extents.y) + 0.3f,
+            controller.bounds.center.z),
+            Vector3.down);
+
+        return Physics.Raycast(groundRay, 0.3f + 0.1f);
+    }
+
+    //Set the Avatar for Player
+    void SetAnimator()
+    {
+        Animator[] animators = GetComponentsInChildren<Animator>();
+        if (animators.Length > 0)
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                Animator anim = animators[i];
+                Avatar av = anim.avatar;
+
+                if (anim != animator)
+                {
+                    animator.avatar = av;
+                    Destroy(anim);
+                }
+            }
+        }
+    }
+
+
+    //For Start Game
+    public void StartGame()
+    {
+        isRunning = true;
+        animator.SetTrigger("startRunning");
+    }
+
+    private void Crash()
+    {
+        animator.SetTrigger("Death");
+        isRunning = false;
+        GameManager.Instance.OnDeath();
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        switch(hit.gameObject.tag)
+        {
+            case "Obstacle":
+                Crash();
+                break;
+        }
+    }
+}
